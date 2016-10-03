9945c3c589011fa0bef75adfb16f45b9270d0cb9
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-03 15:45:51 +0100

    mugs, specs and sausage rolls

--- index.php
+++ index.php
@@ -10,14 +10,20 @@
                 echo "My Name is " . $myname . " and I am" . $myage;
                 echo "Hello World";
                 echo 5 * 7;*/
-            $name = "Edgar";
+            /*$name = "Edgar";
 
             if ($name=="Simon"){
                 print "I know you";
             }
             else{
                 print "I don't know you";
-            }
+            }*/
+            $myage = 12;
+
+            if ($myage>21) print "sausage rolls";
+            else if ($myage>18) print "mugs";
+            else if ($myage>16) print "specs";
+            else print "nothing bitch";
             ?>
         </p>
     </body>
