857cc0f3afe50f73c751570087480b7f6c41d329
William Haynes <William Haynes>
2016-10-03 15:04:16 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "Hello World";
+            ?>
+        </p>
+    </body>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: hype_
+ * Date: 03/10/2016
+ * Time: 14:13
+ */
