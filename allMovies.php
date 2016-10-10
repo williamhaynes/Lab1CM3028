6ca65135b1996ee7ae169785b4109ee0053abb3c
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-10 15:03:52 +0100

    updated index page

--- allMovies.php
+++ allMovies.php
@@ -20,20 +20,7 @@
     // test if connection was established, and print any errors
     if($db->connect_errno){
     die("Connectfailed['.$db->connect_error.']");
-    }
-    // create a SQL query as a string
-    $sql_query = "SELECT * FROM superheros WHERE superpower LIKE '%l aser%' ";
-    // execute the SQL query
-    $result = $db->query($sql_query);
-    // iterate over $resul t obj ect one $row at a time
-    // use fetch_array() to return an associ ative array
-    while($row = $result->fetch_array()){
-     // print out fiel ds from row of data
-     echo "<p>" . $row['superheroName' ] . "</p>";
-    }
-    $result->close();
-     // close connection to database
-     $db->close();
+    };
 
         ?>
 </body>
