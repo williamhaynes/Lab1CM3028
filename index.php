f54f122884466d3f56f2011e57890bee39039151
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-10 14:38:23 +0100

    updated index page

--- index.php
+++ index.php
@@ -11,15 +11,15 @@
         <link href="https://fonts.googleapis.com/css?family=Yellowtail" rel="stylesheet">
     </head>
     <body>
-    <div id="mainBox">
-        Marvel Movies Database
-    </div>
-    <nav id="navigationBar">
-        <li>Display All Movies</li>
-        <li>Display Movies created by Marvel Studios</li>
-        <li>Display All Movies Created after 2010</li>
-        <li>Display All X-Men Movies</li>
-    </nav>
-        <p><?php ?></p>
+        <div id="mainBox">
+            Marvel Movies Database
+        </div>
+        <nav id="navigationBar">
+            <li>Display All Movies</li>
+            <li>Display Movies created by Marvel Studios</li>
+            <li>Display All Movies Created after 2010</li>
+            <li>Display All X-Men Movies</li>
+        </nav>
+            <p><?php ?></p>
     </body>
 </html>
