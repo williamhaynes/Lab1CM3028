fa1511fe26d76491036f237ac316c35c36846edb
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-10 15:15:22 +0100

    updated index page

--- allMovies.php
+++ allMovies.php
@@ -22,7 +22,7 @@
     die("Connectfailed['.$db->connect_error.']");
     };
 
-    $sql_query = "SELECT * FROM superheros WHERE superpower LIKE %aser%";
+    $sql_query = "SELECT * FROM marvelmovies";
     $result = $db->query($sql_query);
     while($row = $result->fetch_array()){
         echo"<p>" . $row['superheroName'] . "</p>";
